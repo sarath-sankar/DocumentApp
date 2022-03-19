42180930e6aa69aec8f8cbebb0956f0f9d82c34f
anishjose27 <anishjose27@gmail.com>
2022-03-19 13:15:25 -0400

    Source Updated

---
+++ SQL Tables/Create dbo.Books.sql
@@ -0,0 +1,36 @@
+﻿USE [bookstore]
+GO
+
+/****** Object: Table [dbo].[Books] Script Date: 3/19/2022 1:13:11 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Books] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        VARCHAR (150) NOT NULL,
+    [Author]      VARCHAR (150) NOT NULL,
+    [Description] VARCHAR (350) NULL,
+    [Value]       FLOAT (53)    NOT NULL,
+    [PublishDate] DATETIME2 (7) NOT NULL,
+    [CategoryId]  INT           NOT NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Books_CategoryId]
+    ON [dbo].[Books]([CategoryId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[Books]
+    ADD CONSTRAINT [PK_Books] PRIMARY KEY CLUSTERED ([Id] ASC);
+
+
+GO
+ALTER TABLE [dbo].[Books]
+    ADD CONSTRAINT [FK_Books_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([Id]);
+
+
---
+++ SQL Tables/Create dbo.Categories.sql
@@ -0,0 +1,16 @@
+﻿USE [bookstore]
+GO
+
+/****** Object: Table [dbo].[Categories] Script Date: 3/19/2022 1:14:21 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Categories] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (150) NOT NULL
+);
+
+
